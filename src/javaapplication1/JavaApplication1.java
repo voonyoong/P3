cda82a797e76ed320e21fc0483d29c5f7f3578cb
Tarc <Tarc@B91>
2018-11-12 11:03:33 +0800

    HIHI

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
     }
     
 }
